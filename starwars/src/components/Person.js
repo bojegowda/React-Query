223dad3cfddf2e15fe485af7f5d8eda94a471547
bojegowda671@gmail.com <bojegowda671@gmail.com>
2020-08-18 17:57:21 +0530

    created starwars app with navigation, planets and people component created

---
+++ starwars/src/components/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+const Planet = ({
+    person
+}) => {
+    return ( <
+        div className = "card" >
+        <
+        h3 > {
+            person.name
+        } < /h3> <
+        p > Gender - {
+            person.gender
+        } < /p> <
+        p > Birth Year - {
+            person.birth_year
+        } < /p> < /
+        div >
+    );
+}
+
+export default Planet
